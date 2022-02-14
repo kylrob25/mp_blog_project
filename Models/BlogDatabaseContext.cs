bd32a1469543ec2d60a1d4e56dfde1dba28b4207
Kyle <kyle.r@techie.com>
2022-02-14 19:54:13 +0000

    Add project files.

---
+++ Models/BlogDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace KRoberts_Theatre_Blog.Models
+{
+    public class BlogDatabaseContext : IdentityDbContext<User>
+    {
+        public BlogDatabaseContext()
+            : base("BlogConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BlogDatabaseContext Create()
+        {
+            return new BlogDatabaseContext();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KRoberts_Theatre_Blog.Startup))]
+namespace KRoberts_Theatre_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
