f6ddb03d209616a34bffd807fecfa8a2aa2e3393
Kyle <kyle.r@techie.com>
2022-02-14 20:00:36 +0000

    Start User class

--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,11 @@
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
+using System;
+using System.Web;
+using Microsoft.AspNet.Identity.Owin;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace KRoberts_Theatre_Blog.Models
 {
@@ -9,8 +14,27 @@ namespace KRoberts_Theatre_Blog.Models
     public class User : IdentityUser
     {
 
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public bool Suspended { get; set; }
+
+        private ApplicationUserManager userManager;
+
+        public string Role
+        {
+            get
+            {
+                if (userManager == null)
+                {
+                    userManager = HttpContext.Current.GetOwinContext().Get<ApplicationUserManager>();
+                }
+
+                return userManager.GetRoles(Id).Single();
+            }
+        }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
